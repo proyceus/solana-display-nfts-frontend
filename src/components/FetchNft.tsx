af0786ac8c8b2cb451d4dcc96768446066acfbfc
proyceus <proyceus@gmail.com>
2022-11-02 15:40:30 -0400

    test

--- src/components/FetchNft.tsx
+++ src/components/FetchNft.tsx
@@ -4,6 +4,7 @@ import { FC, useEffect, useState } from "react"
 import styles from "../styles/custom.module.css"
 
 export const FetchNft: FC = () => {
+  //test
   const fetchNfts = async () => {}
 
   return <div></div>
