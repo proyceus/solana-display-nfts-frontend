00f7111a2a94b7c5aba8642bddda5ceffddbe228
proyceus <proyceus@gmail.com>
2022-11-02 15:41:49 -0400

    remove test

--- src/components/FetchNft.tsx
+++ src/components/FetchNft.tsx
@@ -1,11 +1,10 @@
-import { useConnection, useWallet } from "@solana/wallet-adapter-react"
-import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js"
-import { FC, useEffect, useState } from "react"
-import styles from "../styles/custom.module.css"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js";
+import { FC, useEffect, useState } from "react";
+import styles from "../styles/custom.module.css";
 
 export const FetchNft: FC = () => {
-  //test
-  const fetchNfts = async () => {}
+  const fetchNfts = async () => {};
 
-  return <div></div>
-}
+  return <div></div>;
+};
